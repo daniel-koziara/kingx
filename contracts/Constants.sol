f0af3e33e09c7a1aa3eba302f483474a9b76103d
Daniel Koziara <daniel@impresspro.pl>
2024-02-12 11:31:47 +0100

    dev

--- contracts/Constants.sol
+++ contracts/Constants.sol
@@ -8,7 +8,6 @@ address constant HELLWHALE_OWNER = 0x8add03eafe6E89Cc28726f8Bb91096C2dE139fFb;
 address constant DANIEL_KOZIARA_OWNER = 0x7e603e457d8C0D61351111614ad977315Dfc77aa;
 address constant KRONOS_OWNER = 0x9FEAcbaf3C4277bC9438759058E9E334f866992a;
 
-
 // enums
 enum GenesisTokens {
     KINGX, TITANX
--- contracts/KingX.sol
+++ contracts/KingX.sol
@@ -4,28 +4,38 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
-import "hardhat/console.sol";
 
 import "./Constants.sol";
 
-contract KingX is Context, ERC20  {
+contract KingX is Context, ERC20 {
     using SafeERC20 for IERC20;
 
     IERC20 public titanX;
     address public buyAndBurnAddress;
     address public initialLpAddress;
     uint256 public contractStartTime;
-    address public constant routerAddress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address public constant routerAddress =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     uint256 public constant taxFeePercent = 1;
     mapping(GenesisTokens => uint256) public genesis;
     uint256 public constant MINTING_PERIOD = 17 days;
     uint256 public constant INITIAL_RATE = 1e18; // 1:1 rate
     uint256 public constant FINAL_RATE = 1e17; // 1:0.1 rate
+    // mapping(address => uint256) public balances;
+    // uint256 public totalSupply;
 
     event Mint(address indexed user, uint256 amount, uint256 rate);
-    event GenesisRewardDistributed(uint256 amountDaniel, uint256 amountHellwhale, uint256 amountKronos, GenesisTokens);
+    event GenesisRewardDistributed(
+        uint256 amountDaniel,
+        uint256 amountHellwhale,
+        uint256 amountKronos,
+        GenesisTokens
+    );
 
-    constructor(address _buyAndBurnAddress, address _initialLpAddress) ERC20("KINGX", "KINGX") {
+    constructor(
+        address _buyAndBurnAddress,
+        address _initialLpAddress
+    ) ERC20("KINGX", "KINGX") {
         titanX = IERC20(TITANX_ADDRESS);
         buyAndBurnAddress = _buyAndBurnAddress;
         initialLpAddress = _initialLpAddress;
@@ -35,33 +45,61 @@ contract KingX is Context, ERC20  {
         _mint(initialLpAddress, 20e9 * 1e18);
     }
 
+    // function _update(address from, address to, uint256 value) internal virtual override {
+    //     uint256 feeAmount = 0;
+    //     uint256 transferValue = value;
 
-     function _transfer(address from, address to, uint256 amount) internal override {
-        if (from == routerAddress || to == routerAddress) {
-            uint256 feeAmount = (amount * taxFeePercent) / 100;
-            uint256 amountAfterFee = amount - feeAmount;
+    //     if (from == routerAddress || to == routerAddress) {
+    //         feeAmount = (value * taxFeePercent) / 100;
+    //         transferValue = value - feeAmount;
 
-            genesis[GenesisTokens.KINGX] += feeAmount;
-            super._transfer(from, address(this), feeAmount);
-            super._transfer(from, to, amountAfterFee);
-        } else {
-            super._transfer(from, to, amount);
-        }
-    }
+    //         genesis[GenesisTokens.KINGX] += feeAmount;
+
+    //         super.balances[address(this)] += feeAmount;
+    //     }
+
+    //     if (from == address(0)) {
+    //         totalSupply += transferValue;
+    //     } else {
+    //         uint256 fromBalance = balances[from];
+    //         require(
+    //             fromBalance >= value,
+    //             "ERC20: transfer amount exceeds balance"
+    //         );
+    //         unchecked {
+    //             balances[from] = fromBalance - value;
+    //         }
+    //     }
+
+    //     if (to == address(0)) {
+    //         unchecked {
+    //             totalSupply -= transferValue;
+    //         }
+    //     } else {
+    //         unchecked {
+    //             balances[to] += transferValue;
+    //         }
+    //     }
+
+    //     emit Transfer(from, to, transferValue);
+
+    //     if (feeAmount > 0) {
+    //         emit Transfer(from, address(this), feeAmount);
+    //     }
+    // }
 
     function mint(uint256 titanXAmount) external {
-
         require(
-                block.timestamp > contractStartTime,
-                "KingX_Minting: Minting not allowed yet"
-            );
+            block.timestamp > contractStartTime,
+            "KingX_Minting: Minting not allowed yet"
+        );
 
         require(
             block.timestamp <= contractStartTime + MINTING_PERIOD,
             "KingX_Minting: Minting period has ended"
         );
 
-        uint256 genesisAmount = titanXAmount * 3 / 100;
+        uint256 genesisAmount = (titanXAmount * 3) / 100;
 
         titanX.safeTransferFrom(msg.sender, address(this), genesisAmount);
         genesis[GenesisTokens.TITANX] += genesisAmount;
@@ -75,11 +113,10 @@ contract KingX is Context, ERC20  {
         uint256 elapsedTimeRoundedToFullHours = fullHours * 1 hours;
 
         uint256 rate = INITIAL_RATE -
-        ((INITIAL_RATE - FINAL_RATE) * elapsedTimeRoundedToFullHours) /
-        MINTING_PERIOD;
+            ((INITIAL_RATE - FINAL_RATE) * elapsedTimeRoundedToFullHours) /
+            MINTING_PERIOD;
 
         uint256 mintAmount = (titanXAmount * rate) / 1e18;
-
 
         if (mintAmount > titanXAmount) {
             revert("KingX_MintAmount: Mint amount is too high");
@@ -90,29 +127,31 @@ contract KingX is Context, ERC20  {
         emit Mint(msg.sender, mintAmount, rate);
     }
 
-
-    function distributeGenesisRewards(GenesisTokens token) external  {
+    function distributeGenesisRewards(GenesisTokens token) external {
         uint256 totalAmount = genesis[token];
         require(totalAmount > 0, "KingX_Genesis: No rewards to distribute");
         uint256 amountForDaniel = totalAmount / 2; // 50% for Daniel
         uint256 amountForOthers = totalAmount / 4; // 25% for others
-        console.log(totalAmount, amountForDaniel, amountForOthers);
         // Reset genesis pool for the token
         genesis[token] = 0;
-        
-        if(GenesisTokens.TITANX == token) {
+
+        if (GenesisTokens.TITANX == token) {
             // Safe transfer rewards
             titanX.safeTransfer(DANIEL_KOZIARA_OWNER, amountForDaniel);
             titanX.safeTransfer(HELLWHALE_OWNER, amountForOthers);
             titanX.safeTransfer(KRONOS_OWNER, amountForOthers);
-        } else if(GenesisTokens.KINGX == token) {
+        } else if (GenesisTokens.KINGX == token) {
             // Safe transfer rewards
             transfer(DANIEL_KOZIARA_OWNER, amountForDaniel);
             transfer(HELLWHALE_OWNER, amountForOthers);
             transfer(KRONOS_OWNER, amountForOthers);
         }
 
-        emit GenesisRewardDistributed(amountForDaniel, amountForOthers, amountForOthers, token);
+        emit GenesisRewardDistributed(
+            amountForDaniel,
+            amountForOthers,
+            amountForOthers,
+            token
+        );
     }
-
 }
