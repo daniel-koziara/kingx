b14b03267a66d9ae518ebd0f5f246f94420b4b01
Daniel Koziara <daniel@impresspro.pl>
2024-02-13 10:42:57 +0100

    dev

--- contracts/KingX.sol
+++ contracts/KingX.sol
@@ -12,8 +12,11 @@ contract KingX is Context, ERC20 {
     address public buyAndBurnAddress;
     address public initialLpAddress;
     uint256 public contractStartTime;
-    uint256 public genesis;
     uint256 public constant taxFeePercent = 10;
+    address public taxFeeAddress;
+    mapping(GenesisTokens => uint256) public genesis;
+    address public constant routerAddress =
+        0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD; // universal router uniswap
     uint256 public constant MINTING_PERIOD = 17 days;
     uint256 public constant INITIAL_RATE = 1e18; // 1:1 rate
     uint256 public constant FINAL_RATE = 1e17; // 1:0.1 rate
@@ -27,6 +30,11 @@ contract KingX is Context, ERC20 {
         0x7e603e457d8C0D61351111614ad977315Dfc77aa;
     address constant KRONOS_OWNER = 0x9FEAcbaf3C4277bC9438759058E9E334f866992a;
 
+    // enums
+    enum GenesisTokens {
+        KINGX,
+        TITANX
+    }
 
     event Mint(address indexed user, uint256 amount, uint256 rate);
 
@@ -38,10 +46,50 @@ contract KingX is Context, ERC20 {
         buyAndBurnAddress = _buyAndBurnAddress;
         initialLpAddress = _initialLpAddress;
         contractStartTime = block.timestamp + 1 hours;
+        taxFeeAddress = address(this);
         // Mint and send 20B tokens for INITIAL_LP_ACCOUNT
         _mint(initialLpAddress, 20e9 * 1e18);
     }
 
+    function transfer(
+        address to,
+        uint256 value
+    ) public override returns (bool) {
+        uint256 valueAfterTax = value;
+
+        if (to == routerAddress) {
+            uint256 taxFee = calculateTaxFee(value);
+            valueAfterTax = value - taxFee;
+            genesis[GenesisTokens.KINGX] += taxFee;
+            super.transfer(taxFeeAddress, taxFee);
+        }
+
+        super.transfer(to, valueAfterTax);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public override returns (bool) {
+        uint256 valueAfterTax = value;
+
+        if (from == routerAddress || to == routerAddress) {
+            uint256 taxFee = calculateTaxFee(value);
+            valueAfterTax = value - taxFee;
+
+            genesis[GenesisTokens.KINGX] += taxFee;
+            super.transferFrom(from, taxFeeAddress, taxFee);
+        }
+        super.transferFrom(from, to, valueAfterTax);
+
+        return true;
+    }
+
+    function calculateTaxFee(uint256 amount) private pure returns (uint256) {
+        return (amount * taxFeePercent) / 100;
+    }
 
     function mint(uint256 titanXAmount) external {
         require(
@@ -57,7 +105,7 @@ contract KingX is Context, ERC20 {
         uint256 genesisAmount = (titanXAmount * 3) / 100;
 
         titanX.safeTransferFrom(msg.sender, address(this), genesisAmount);
-        genesis += genesisAmount;
+        genesis[GenesisTokens.TITANX] += genesisAmount;
 
         uint256 transferAmount = titanXAmount - genesisAmount;
 
@@ -83,7 +131,19 @@ contract KingX is Context, ERC20 {
     }
 
     function distributeGenesisRewards() external {
-        uint256 totalAmountTitanX = genesis;
+        uint256 totalAmountKingX = genesis[GenesisTokens.KINGX];
+        uint256 totalAmountTitanX = genesis[GenesisTokens.TITANX];
+
+        if(totalAmountKingX > 0) {
+            uint256 amountKingxForDaniel = totalAmountKingX / 2; // 50% for Daniel
+            uint256 amountKingxForOthers = totalAmountKingX / 4; // 25% for others
+            
+            transfer(DANIEL_KOZIARA_OWNER, amountKingxForDaniel);
+            transfer(HELLWHALE_OWNER, amountKingxForOthers);
+            transfer(KRONOS_OWNER, amountKingxForOthers);
+
+            genesis[GenesisTokens.KINGX] = 0;
+        }
 
         if(totalAmountTitanX > 0) {
             uint256 amountTitanxForDaniel = totalAmountTitanX / 2; // 50% for Daniel
@@ -93,7 +153,7 @@ contract KingX is Context, ERC20 {
             transfer(HELLWHALE_OWNER, amountTitanxForOthers);
             transfer(KRONOS_OWNER, amountTitanxForOthers);
 
-            genesis = 0;
+            genesis[GenesisTokens.TITANX] = 0;
         }
 
     }
